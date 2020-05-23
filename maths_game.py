27dc2d54b0e6bae4dd5e21432760b90325640b1e
khuzema-AI <khuzemaahmed26@outlook.com>
2020-05-24 03:29:24 +0500

    commit to update

--- maths_game.py
+++ maths_game.py
@@ -2,23 +2,26 @@ print ('Hy!')
 
 print ('Its Maths Game!')
 
-A1 = 4+4
+def mathGame():
+    A1 = 4+4
 
-answer = input("You in?")
+    answer = input("You in?")
 
-if answer == 'yes':
-    print ('good!')
-    print ('Challenge 1')
-    Q1 = int(input('Q1: 4+4=?'))
-    
-    if Q1==A1:
-        print ('Correct!')
+    if answer == 'yes' or 'yeah' :
+        print ('good!')
+        print ('Challenge 1')
+        Q1 = int(input('Q1: 4+4=?'))
+        
+        if Q1==A1:
+            print ('Correct!')
+        else:
+            print ('Nopes Try Next Time!')
+
     else:
-        print ('Nopes Try Next Time!')
+        print ('Its okay, Bye')
 
-else:
-    print ('Its okay, Bye')
-    
+mathGame()
+
 
 
 
