f5ae0c6b62cdf08c499acdf15b0f7a2ac62583c8
Khuzema <khuzemaahmed26@outlook.com>
2021-02-09 02:14:05 +0500

    renew files

---
+++ table_of_number for loop.py
@@ -0,0 +1,11 @@
+print ('Hy!')
+
+b = int(input('Your Number?'))
+
+print ('Table of ' , b)
+
+for a:
+    a=0
+    if a>10 : break
+    a+=1
+    print (b ,'*', a , '=' , b * a)
---
+++ table_of_number while loop.py
@@ -0,0 +1,12 @@
+print ('Hy!')
+
+b = int(input('Your Number?'))
+
+print ('Table of ' , b)
+
+a = 0
+
+while b:
+    a += 1
+    if a>10 :break
+    print (b ,'*', a , '=' , b * a)
