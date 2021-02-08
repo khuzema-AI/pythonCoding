2773878c8e9e9cdab42c24943c5c08d0291c58ae
Khuzema <khuzemaahmed26@outlook.com>
2021-02-09 02:42:02 +0500

    python is easy

---
+++ Python is Easy/Homework 1 Variables/main.py
@@ -0,0 +1,35 @@
+
+# hello
+# I am Khuzema Ahmed from Pakistan
+
+
+# My Favourite Poetry
+
+# Name of Poetry
+poetryName = "Wo kamal e husn e huzoor hen"
+
+# Writer
+poetryWriter = "Imam Ahmed Raza Khan"
+
+# genre
+poetryGenre = "Naat e Rasool"
+
+# Lyrics
+poetryLyrics = 16
+
+# Duration
+durationInMinutes = "10 to 15"
+
+# Language
+poetryLanguage = "urdu"
+
+# Book
+poetryBook = "Hadaiq e Bakshish Maktaba tul Madinah"
+
+print (poetryName)
+print (poetryWriter)
+print (poetryGenre)
+print (poetryLyrics)
+print (durationInMinutes)
+print (poetryLanguage)
+print (poetryBook)
